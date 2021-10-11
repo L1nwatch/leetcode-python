969e541acce131e1ee320bdbaa7ecd513537ca8b
L1nwatch <watch1602@gmail.com>
2021-10-11 14:49:08 +0800

    create dictionary

--- 58.最后一个单词的长度/58.最后一个单词的长度.py
+++ 58.最后一个单词的长度/58.最后一个单词的长度.py
@@ -4,7 +4,7 @@
 # [58] 最后一个单词的长度
 #
 
-# @lc code=start
+# @lc code=startaaa
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
         s = s.rstrip() 
--- 67.二进制求和/67.二进制求和.py
+++ 67.二进制求和/67.二进制求和.py
@@ -7,7 +7,6 @@
 # @lc code=start
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        answer = int(a,2)+int(b,2)
+        answer = int(a, 2)+int(b, 2)
         return bin(answer)[2:]
 # @lc code=end
-
