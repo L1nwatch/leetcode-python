b992892e6b6e457e4669493c149e091b9739f829
L1nwatch <490772448@qq.com>
2022-02-12 15:23:42 +0800

    add 157

---
+++ 157. 用 Read4 读取 N 个字符.py
@@ -0,0 +1,32 @@
+"""
+The read4 API is already defined for you.
+
+    @param buf4, a list of characters
+    @return an integer
+    def read4(buf4):
+
+# Below is an example of how the read4 API can be called.
+file = File("abcdefghijk") # File is "abcdefghijk", initially file pointer (fp) points to 'a'
+buf4 = [' '] * 4 # Create buffer with enough space to store characters
+read4(buf4) # read4 returns 4. Now buf = ['a','b','c','d'], fp points to 'e'
+read4(buf4) # read4 returns 4. Now buf = ['e','f','g','h'], fp points to 'i'
+read4(buf4) # read4 returns 3. Now buf = ['i','j','k',...], fp points to end of file
+"""
+
+class Solution:
+    def read(self, buf, n):
+        """
+        :type buf: Destination buffer (List[str])
+        :type n: Number of characters to read (int)
+        :rtype: The number of actual characters read (int)
+        """
+        answer = 0
+        for i in range(0,n,4):
+            temp_list = [""]*4
+            read_length = read4(temp_list)
+            if read_length == 0:
+                break
+            for j in range(read_length):
+                buf[answer] = temp_list[j]
+                answer += 1
+        return min(n, answer)
