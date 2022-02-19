f2cc93acea0b7e99b1726b87fc6933da204a4121
watch <490772448@qq.com>
2022-02-19 13:45:38 +0800

    update 19/876

--- 19.删除链表的倒数第N个节点/solve.py
+++ 19.删除链表的倒数第N个节点/solve.py
@@ -37,3 +37,49 @@ class Solution:
             node_map[i - n].next = node_map[i - n + 2]
 
         return head
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        length = 0
+        current_node = head
+        while current_node:
+            current_node = current_node.next
+            length += 1
+
+        if length == 1:
+            return None
+        if length == n:
+            head = head.next
+            return head
+
+        target = length - n
+        current_node = head
+        for i in range(target-1):
+            current_node = current_node.next
+        else:
+            current_node.next = current_node.next.next
+        return head
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        answer = ListNode(0, head)
+        slow, fast = answer, head
+        while n > 0:
+            fast = fast.next
+            n -= 1
+        
+        while fast:
+            slow = slow.next
+            fast = fast.next
+        slow.next = slow.next.next
+        return answer.next
--- 876.链表的中间结点.py
+++ 876.链表的中间结点.py
@@ -26,3 +26,34 @@ class Solution:
         return current_node
 # @lc code=end
 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: ListNode) -> ListNode:
+        length = 0
+        current_node = head
+        while current_node is not None:
+            length += 1
+            current_node = current_node.next
+
+        answer_node = head
+        middle = length // 2 + 1
+        for i in range(1, middle):
+            answer_node = answer_node.next
+        return answer_node
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: ListNode) -> ListNode:
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
