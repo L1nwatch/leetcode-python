0f87991a73a339593ccd34ad6c07933c128c897e
L1nwatch <watch1602@gmail.com>
2022-03-07 15:04:28 +0800

    update 567/update 695

--- 567. Permutation in String.py
+++ 567. Permutation in String.py
@@ -17,3 +17,25 @@ class Solution:
             if count_s1 == count_s2:
                 return True
         return False
+
+class Solution2:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        length_s1 = len(s1)
+        length_s2 = len(s2)
+        if length_s1 > length_s2:
+            return False
+
+        from collections import Counter
+        counter_s1 = Counter(s1)
+        counter_substring = Counter(s2[:length_s1])
+        if counter_s1 == counter_substring:
+            return True
+
+
+        for i in range(length_s1,length_s2):
+            new_char,old_char = s2[i],s2[i-length_s1]
+            counter_substring[new_char] = counter_substring.get(new_char,0)+1
+            counter_substring[old_char] -= 1
+            if counter_substring == counter_s1:
+                return True
+        return False
--- 695. Max Area of Island.py
+++ 695. Max Area of Island.py
@@ -26,3 +26,27 @@ class Solution:
                         area += 1
             answer = max(answer,area)
         return answer
+
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        answer = 0
+        m,n = len(grid),len(grid[0])
+        from collections import deque
+        points = [(i,j) for i in range(m) for j in range(n) if grid[i][j] == 1]
+        points = deque(points)
+        seen = set()
+        while points:
+            x,y = points.pop()
+            if (x,y) not in seen:
+                area_deque = deque([(x,y)])
+                area = 1
+                seen.add((x,y))
+                while area_deque:
+                    x,y = area_deque.pop()
+                    for i,j in [(x+1,y),(x-1,y),(x,y+1),(x,y-1)]:
+                        if 0 <= i < m and 0 <= j < n and grid[i][j] == 1 and (i,j) not in seen:
+                            area += 1
+                            area_deque.append((i,j))
+                            seen.add((i,j))
+                answer = max(area,answer)
+        return answer
