92d1845c46bdf475c5b2e6ce83fb147384240c35
watch <490772448@qq.com>
2022-03-10 19:42:54 +0800

    update 21

--- 21.合并两个有序链表/solve.py
+++ 21.合并两个有序链表/solve.py
@@ -81,3 +81,31 @@ class Solution:
             current_node = current_node.next
             list2 = list2.next
         return head
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution2:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if not list2:
+            return list1
+        if not list1:
+            return list2
+        
+        head = ListNode()
+        current_node = head
+        while list1 and list2:
+            if list1.val <= list2.val:
+                current_node.next = list1
+                list1 = list1.next
+            else:
+                current_node.next = list2
+                list2 = list2.next
+            current_node = current_node.next
+        if list1:
+            current_node.next = list1
+        elif list2:
+            current_node.next = list2
+        return head.next
