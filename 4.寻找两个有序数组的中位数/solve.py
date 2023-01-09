0fcf9e3cb24a5835987f58d1a0e30b8ffd22f614
L1nwatch <490772448@qq.com>
2023-01-09 16:41:30 +0800

    [2023-01-09] wrong answer, need to correct

--- 4.寻找两个有序数组的中位数/solve.py
+++ 4.寻找两个有序数组的中位数/solve.py
@@ -9,37 +9,8 @@ __author__ = '__L1n__w@tch'
 
 
 class Solution:
-    def longestPalindrome(self, s: str) -> str:
-        # 动态规划
-        length = len(s)
-        if length <= 1:
-            return s
-        dp = [[False for l in range(length)] for r in range(length)]
-        max_length = 0
-        result = s[0]
-        for r in range(1, length):
-            for l in range(r):
-                if s[l] == s[r] and ((r - l) == 1 or (r - l) == 2 or dp[l + 1][r - 1]):
-                    dp[l][r] = True
-                    if (r - l + 1) > max_length:
-                        max_length = r - l + 1
-                        result = s[l:r + 1]
-        return result
+    pass
 
 
 class Solution1:
-    def is_huiwen(self, string):
-        return string == string[::-1]
-
-    def longestPalindrome(self, s: str) -> str:
-        max_s = ""
-        for i in range(len(s)):
-            temp_s = ""
-            temp_max = ""
-            for j in range(i, len(s)):
-                temp_s += s[j]
-                if self.is_huiwen(temp_s) and len(temp_s) > len(temp_max):
-                    temp_max = temp_s
-            if len(temp_max) > len(max_s):
-                max_s = temp_max
-        return max_s
+    pass
--- 4.寻找两个有序数组的中位数/test_solve.py
+++ 4.寻找两个有序数组的中位数/test_solve.py
@@ -11,13 +11,7 @@ __author__ = '__L1n__w@tch'
 
 class TestSolution(unittest.TestCase):
     def test_solve_func(self):
-        test_data_list = ["babcd","cbbd","abb","aacdefcaa"]
-        right_answer_list = ["bab","bb","bb","aa"]
-
-        for test_data, right_answer in zip(test_data_list, right_answer_list):
-            s = Solution()
-            my_answer = s.longestPalindrome(s=test_data)
-            self.assertEqual(my_answer, right_answer)
+        pass
 
 
 if __name__ == "__main__":
